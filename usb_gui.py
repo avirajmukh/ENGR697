057665f3a01bf1bc15a9e58423dcd58551d1ab40
avirajmukh <aviraj.mukh@gmail.com>
2018-04-29 13:41:37 -0700

    added fullscreen, escape-to-quit and comments

--- usb_gui.py
+++ usb_gui.py
@@ -3,9 +3,11 @@ from tkinter import *
 from guizero import App, Text, PushButton
 from subprocess import call
 
+# Setting initial boolean values to check for the file and directory selection
 file_check = False
 dir_check = False
 
+# Opens a file dialog to select a file in the filesystem
 def select_file():
     global filename
     filename =  filedialog.askopenfilename(initialdir = "C:\\", title = "Select file")
@@ -13,6 +15,7 @@ def select_file():
     global file_check
     file_check = True
 
+# Opens a file dialog to select a directory in the filesystem
 def select_dir():
     global directory
     directory =  filedialog.askdirectory()
@@ -20,6 +23,7 @@ def select_dir():
     global dir_check
     dir_check = True
 
+# runs the rsync linux command to sync files to destination directories
 def use_rsync():
     if (file_check and dir_check):
         sync_text.value = "File sent to destination"
@@ -29,10 +33,16 @@ def use_rsync():
     else:
         sync_text.value = "Please select a source file and destination folder"
 
+#This function destroys and exits the program when called
+def on_escape(event=None):
+    print("Exiting program")
+    app.tk.destroy()
+
 app = App(title="USB Transfer Device", width=480, height=320) 
 #   480x320 should be the resolution of the touch screen)
 welcome_message = Text(app, text="Please choose a source file and a destination directory")
 
+#   Establishes buttons for file / directory selection and sync
 file_button = PushButton(app, command=select_file, text="Select a source file", grid=[0,1])
 curr_file = Text(app, text="Current file: ", grid=[1,1])
 
@@ -42,6 +52,7 @@ curr_dir = Text(app, text="Current directory: ", grid=[3,1])
 sync_button = PushButton(app, command=use_rsync, text="Sync Files to Destination", grid=[5,1])
 sync_text = Text(app, text="", grid=[6,1])
 
-
+app.tk.attributes("-fullscreen", True)  #force the app to run in fullscreen
+app.tk.bind("<Escape>", on_escape)      #Binds the escape key on a keyboard to the on_escape function
 app.display()
 
